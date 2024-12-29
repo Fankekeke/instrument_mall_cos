ceecbefbdf60d95347a8131870199a9ec18fa7f7
Fankekeke <1258846421@qq.com>
2024-12-29 21:49:28 +0800

    fix - 小程序修改

--- frontend/src/store/modules/setting.js
+++ frontend/src/store/modules/setting.js
@@ -12,7 +12,7 @@ export default {
     isMobile: false,
     theme: db.get('THEME', 'light'),
     layout: db.get('LAYOUT', 'side'),
-    systemName: '家庭电器购买平台',
+    systemName: '乐器商城平台',
     copyright: `${new Date().getFullYear()} <a>FanK</a>`,
     multipage: getBooleanValue(db.get('MULTIPAGE'), true),
     fixSiderbar: getBooleanValue(db.get('FIX_SIDERBAR'), true),
